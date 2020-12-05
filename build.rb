a21cb2d6e94d928882dd79a807d14e49a0960934
Tiffany White <hey@tiffanyrwhite.com>
2020-12-04 19:36:02 -0500

    Fix methods and add variables

--- build.rb
+++ build.rb
@@ -1,32 +1,45 @@
 prod_build = ARGV[0] = "production_build"
 
+# Capture input
+
+print "Enter page name: "
+page = gets
+
+print "Enter script name: "
+script = gets
+
+print "Enter style name: "
+style = gets
+
 # Read files
 
-def page_partials(new_page_partial)
-  pages = "site/#{page_partial}.html"
-  new_page_partial = File.open(pages).read
+def page_partials(page)
+  pages = "site/#{page}.html"
+  page = File.open(pages).read
 end
 
-def script_partials(new_script_partial)
-  scripts = "site/#{script_partial}.js"
-  new_script_partial = File.open(scripts).read
+def script_partials(script)
+  scripts = "site/#{script}.js"
+  script = File.open(scripts).read
 end
 
-def style_partial(new_style_partial)
-  styles = "site/#{style_partial}.css"
-  new_style_partial(styles).read
+def style_partial(style)
+  styles = "site/#{style}.css"
+  style = File.open(styles).read
 end
 
-puts page_partials()
+puts page_partials(page)
+puts script_partials(script)
+puts style_partials(style)
 
 head_html   = File.open("site/_head.html").read
 seo_html    = File.open("site/_seo.html").read
 main_css    = File.open("site/_main.css").read
-body_html   = File.open("site/_body.html").read
-scaffold_js = File.open("site/_scaffold.js").read
-scripts_js  = File.open("site/_scripts.js").read
-base_html   = File.open("site/base.html").read
-posts_html   = File.open("site/posts.html").read
+# body_html   = File.open("site/_body.html").read
+# scaffold_js = File.open("site/_scaffold.js").read
+# scripts_js  = File.open("site/_scripts.js").read
+# base_html   = File.open("site/base.html").read
+# posts_html   = File.open("site/posts.html").read
 dev_html    = ""
 
 
@@ -37,14 +50,9 @@ end
 # Create page
 
 build_string = base_html
-  .gsub("{{ head }}", head_html)
+  .gsub("{{ head }}", "#{page_partials}")
   .gsub("{{ seo }}", seo_html)
   .gsub("{{ main }}", main_css)
-  .gsub("{{ body }}", body_html)
-  .gsub("{{ scaffold }}", scaffold_js)
-  .gsub("{{ scripts }}", scripts_js)
-  .gsub("{{ base }}", base_html)
-  .gsub("{{ posts }}", posts_html)
   .gsub("{{ dev }}", dev_html)
 
   # Write to index page
